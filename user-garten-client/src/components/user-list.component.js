fb1dab6bb6c923b0df1a1a9bf2c9c58e1ff3216a
Oleg Kazantsev <ok020373@gmail.com>
2020-01-24 18:13:40 +1300

    User list component draft

--- user-garten-client/src/components/user-list.component.js
+++ user-garten-client/src/components/user-list.component.js
@@ -1,19 +1,97 @@
 import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import axios from 'axios';
+import Title from "./title.component";
 
 export default class UserList extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            queryText: "",
+            evidence: [],
+            columns: [{
+                dataField: 'title',
+                text: 'Title',
+                sort: true
+            }, {
+                dataField: 'authors',
+                text: 'Authors',
+                formatter: a => a.map(ai => ai.lastName + ', ' + ai.firstName).join('; '),
+                sort: true
+            }, {
+                dataField: 'keywords',
+                text: 'Keywords',
+                formatter: k => k.join(', '),
+                sort: true
+            },
+            {
+                dataField: 'date',
+                text: 'Publish Date',
+                formatter: a => {
+                    try {
+                        return new Intl.DateTimeFormat('en-NZ', {
+                            year: 'numeric',
+                            month: 'short'
+                        }).format(new Date(a));
+                    }
+                    catch {
+                        return 'N/A';
+                    }
+                },
+                sort: true
+            },
+            {
+                dataField: 'method',
+                text: 'SE method',
+                sort: true
+            },
+            {
+                dataField: 'researchQuestion',
+                text: 'Research Question',
+                sort: true
+            },
+            {
+                dataField: 'result',
+                text: 'Result',
+                sort: true
+            },
+            ],
+            searchResult: []
+        }
+    }
+
+    onSearch = () => {
+        // Get search criteria
+
+        // -- Title
+
+        // -- Calendar
+
+        // -- Filters
+
+
+        // Prepare query
+
+        // Run query
+        axios.get('/api/v1/evidences')
+            .then(response => {
+                this.setState({ searchResult: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>
-                        User List
-                  </h1>
-                </header>
+            <div>
+                <Title callback={this.onSearch} />
+                <div className="py-3">
+                    <BootstrapTable keyField='_id'
+                        data={this.state.searchResult}
+                        columns={this.state.columns} />
+                </div>
             </div>
         );
     }
