0bb42086f9d3e9c98053f9a2c69f6a0ef4964555
Oleg Kazantsev <ok020373@gmail.com>
2020-01-24 16:51:42 +1300

    Main menu, about, base styles

--- user-garten-client/src/App.js
+++ user-garten-client/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './ok-logo.png';
+
+import About from "./components/about.component";
+import UserList from "./components/user-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="http://localhost:3000/" target="_blank">
+              <img src={logo} width="30" height="30" />
+            </a>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/user-list" className="nav-link">User List</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/about" className="nav-link">About</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <Route path="/" exact component={UserList} />
+          <Route path="/user-list" component={UserList} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ user-garten-client/src/components/user-list.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class UserList extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>
+                        User List
+          </h1>
+                </header>
+            </div>
+        );
+    }
+}
